e973fd24837d5ae9146c91d82da843ede0e6c2f6
vandanj01 <vandan.jain.01@gmail.com>
2025-08-07 00:05:42 +0530

    implement recent sessions table

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,52 @@
 import React from 'react'
-import { Button } from "@/components/ui/button";
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
+import { recentSessions } from '@/constants';
 
 const Page = () => {
   return (
-    <div>
-      <h1 className = "text-2xl underline">Welcome to my Saas App!</h1>
-      <Button>
-         Let's get started!
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      
+      <section className = "home-section">
+          <CompanionCard 
+            id = "123"
+            name = "Neura the Brainy Explorer"
+            topic = "Neural Network of the Brain"
+            subject = "science"
+            duration = {45}
+            color = "#ffdda6"  
+          />
+
+          <CompanionCard 
+            id = "456"
+            name = "Countsy the Number Wizard"
+            topic = "Derivatives & Integrals"
+            subject = "maths"
+            duration = {30}
+            color = "#e5d0ff" 
+          />
+
+          <CompanionCard 
+            id = "789"
+            name = "Verba the Vocabulary Builder"
+            topic = "language"
+            subject = "English Literature"
+            duration = {30}
+            color = "#bde7ff"  
+          />
+      </section>
+
+      <section className = "home-section">
+          <CompanionsList 
+            title = "Recently Completed Sessions"
+            companions = { recentSessions }
+            classNames = "w-2/3 max-lg:w-full"
+          />
+          <CTA />
+      </section>
+    </main>
   )
 }
 
