3cddc4cd609d3640b8bfef5316b4915082e87157
vandanj01 <vandan.jain.01@gmail.com>
2025-08-08 15:39:50 +0530

    implement search and filter in companion library

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,6 +1,58 @@
+'use client';
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { subjects } from "@/constants";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
 const SubjectFilter = () => {
+    
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const query = searchParams.get('subject') || '';
+
+    const [subject, setSubject] = useState(query);
+
+    useEffect(() => {
+        let newUrl = "";
+        if(subject === "All") {
+            newUrl = removeKeysFromUrlQuery({
+                params: searchParams.toString(),
+                keysToRemove: ['subject'],
+            });
+        }
+        else {
+            newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: 'subject',
+                value: subject,
+            })
+        }
+
+        router.push(newUrl, { scroll: false });
+    }, [subject]);
+
     return (
-        <div>SubjectFilter</div>
+        <Select onValueChange={setSubject} value = { subject }>
+            <SelectTrigger className="input capitalize">
+                <SelectValue placeholder="Subject" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="All">All Subjects</SelectItem>
+                {subjects.map((subject) => (
+                    <SelectItem key = { subject } value = { subject } className = "capitalize">
+                        { subject }
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
     );
 }
 
